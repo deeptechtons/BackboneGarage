cf7416f41f59754413c4e6ce949c3fd5469588cc
deeptechtons <deeptechtons@gmail.com>
2013-02-13 21:10:53 +0530

    the very important model widget root cause for the projects existence :)

---
+++ Widget/model/widget.js
@@ -0,0 +1,54 @@
+﻿/*
+
+Defines the Widget model that defines how the widget looks
+
+*/
+
+
+define(function () {
+
+    var widget = Backbone.Model.extend({
+        defaults: function () {
+            return {
+                title: 'Widget Title',
+                content: 'Hi I am widget',
+                disabled: false
+            };
+        },
+        validate: function (changedProperties) {
+            /*
+            changed properties
+            [attributes,changes:{id: true}]
+            */
+            return 0;
+            var errorList = [];
+            var model = _.extend(changedProperties, this.attributes);
+            if (!model.Title) {
+                errorList.push({ Title: "Title is required" });
+            }
+            if (!model.Content) {
+                errorList.push({ Content: "Content is required" });
+            }
+            console.log(arguments, 'Widget Validate Event');
+            return errorList;
+        },
+        disable: function () {
+            //disable only when enabled
+            if (!this.get("disabled")) {
+                this.set({ "disabled": true });
+                this.save();
+            }
+        },
+        enable: function () {
+            //enable only when disabled
+            if (this.get("disabled")) {
+                this.set({ "disabled": false });
+                this.save();
+            }
+        },
+        isEnabled: function () {
+            return !this.get("disabled");
+        }
+    });
+    return widget;
+});
