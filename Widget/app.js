7bc7500fee0887c3eecc8dd095f44f1ee951bdb0
deeptechtons <deeptechtons@gmail.com>
2013-02-13 20:43:18 +0530

    Application Kick Start File

---
+++ Widget/app.js
@@ -0,0 +1,32 @@
+﻿requirejs.config({
+    //load all required files from the current folder
+    baseUrl: ".",
+    paths: {
+        widgetModel: "model/widget",
+        widgetView: "views/widget",
+        widgetsView: "views/widgets",
+        widgets: "collection/widgets",
+        appView: "views/app",
+        widgetCreate: "views/create",
+        text: "../library/text",
+        localStore: "../library/store",
+        disabledWidgets:"views/disabledWidget"
+    }
+});
+
+
+requirejs(["appView", "widgets", 'widgetCreate', 'widgetsView', 'disabledWidgets'], function (applicationView, widgetCollection, widgetCreateView, widgetsView, disabledWidgetListView) {
+    
+    var widgets = new widgetCollection();
+
+    new applicationView().render().$el.appendTo("body");
+
+    new disabledWidgetListView({ collection: widgets }).render().$el.appendTo("#DisabledWidgetList");
+
+    new widgetCreateView({ collection: widgets }).render().$el.appendTo("#CreateWidget");
+
+    new widgetsView({ collection: widgets }).render().$el.appendTo("#widgetContainer");
+
+    widgets.fetch();
+
+});
