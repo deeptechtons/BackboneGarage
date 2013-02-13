95d6af77a8303e21bd2940694301111ff41f387b
deeptechtons <deeptechtons@gmail.com>
2013-02-13 21:06:22 +0530

    Widget Create View

---
+++ Widget/views/create.js
@@ -0,0 +1,33 @@
+﻿
+
+
+define(['text!templates/create.html', 'widgetModel'], function (createTemplate, widget) {
+
+    var createWidgetView = Backbone.View.extend({
+        template: createTemplate,
+        tagName: "div",
+        className: "well",
+        initialize: function () {
+            _.bindAll(this);
+        },
+        render: function () {
+            this.$el.html(this.template);
+            return this;
+        },
+        events: {
+            'click #btnCreateWidget': 'addWidget'
+        },
+        addWidget: function () {
+            var instance = new widget();
+            instance.set({
+                title: this.$el.find('#txtWidgetName').val(),
+                content: this.$el.find('#txtWidgetContent').val(),
+                disabled:false
+            });
+            this.collection.addWidget(instance);
+        }
+    });
+
+    return createWidgetView;
+
+});
