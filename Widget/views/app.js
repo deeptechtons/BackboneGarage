947e162b1c2796afb2ae2c73909e35961037f7d1
deeptechtons <deeptechtons@gmail.com>
2013-02-13 21:05:34 +0530

    Application View

---
+++ Widget/views/app.js
@@ -0,0 +1,27 @@
+﻿/*
+
+Defines the Application view that Initializes the Place Holders
+
+*/
+
+
+define(["text!templates/app.html"], function (applicationTemplate) {
+
+    var applicationView = Backbone.View.extend({
+        template: $(applicationTemplate).html(),
+        tagName: 'div',
+        className: 'container',
+        initialize: function () {
+        },
+        render: function () {
+            this.$el.html(this.template);
+            return this;
+        },
+        events: {
+
+        }
+    });
+
+    return applicationView;
+
+});
