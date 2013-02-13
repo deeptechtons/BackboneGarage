40ff3510ff904a180d1ca24ebea758fa7aefed87
deeptechtons <deeptechtons@gmail.com>
2013-02-13 20:35:11 +0530

    Widget Application requirejs optimizer Build Configuration File

---
+++ Widget/build.js
@@ -0,0 +1,31 @@
+﻿//Build Command
+//node r.js -o build.js
+({
+    baseUrl: ".",
+    //paths to modules
+    paths: {
+        widgetModel: "model/widget",
+        widgetView: "views/widget",
+        widgetsView: "views/widgets",
+        widgets: "collection/widgets",
+        appView: "views/app",
+        widgetCreate: "views/create",
+        text: "../library/text",
+        localStore: "../library/store",
+        disabledWidgets: "views/disabledWidget"
+    },
+    //load text plugin and local storage dynamically
+    excludeShallow:["text","localStore"],
+    //name of optimized file
+    name: "app",
+    //single file build
+    out:"app-build.js",
+    /*
+    //deploy to release folder`
+    dir: "release",
+    */
+    //make sure html files are excluded
+    inlineText:false,
+    //remove the build directory and then deploy
+    keepBuildDir: false
+})
